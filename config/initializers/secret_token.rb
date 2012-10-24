c56fda550c7d0ddc885f40643837ae1177cf6ffa
Fred <fred.estabrook@gmail.com>
2012-10-23 23:26:48 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeSheep::Application.config.secret_token = 'd12089edcdc34524db506e103acaeab138cc68c3b1b7c7bc095a6e37aa1951e203bd006f1df0d099d04feebb686240f5ae009a242c73bfdc88b055fbd8088390'
